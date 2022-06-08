d686005b914fc709d5cc9000f13b0e7f6164d5ae
simonasablinaite <simonasablinaite@gmail.com>
2022-06-08 08:49:09 +0300

    export-import exsample

---
+++ index.js
@@ -0,0 +1,12 @@
+import { object } from './object.js';
+
+console.log(object);
+console.log(object.name);
+
+let lexus = new Object;
+console.log(lexus);
+
+lexus.fuelType = 'hybrid';
+console.log(lexus);
+
+
---
+++ object.js
@@ -0,0 +1,7 @@
+const object = {
+    name: 'Lexus',
+    age: 2016,
+    gearbox: 'automatic'
+}
+
+export { object };
